c4b775b9a692715886839b0188c3e104fff87511
Nikolay Malyutin <xenos-nick@mail.ru>
2021-12-18 10:28:38 +0300

    Функции по каждому рецепту заменены общей функцией recipes

--- calculator/views.py
+++ calculator/views.py
@@ -19,37 +19,14 @@ DATA = {
 }
 
 
-def omlet(request):
+def recipe(request, dish):
     servings = int(request.GET.get("servings", 1))
+    count_list = [ingredient * servings for ingredient in list(DATA[dish].values())]
+    DATA[dish] = dict(zip(list(DATA[dish].keys()), count_list))
     context = {
-        'recipe': {
-            'яйца, шт': 2 * servings,
-            'молоко, л': 0.1 * servings,
-            'соль, ч.л.': 0.5 * servings,
-        }
+        'recipe':
+            DATA[dish]
     }
     return render(request, 'calculator/index.html', context)
 
 
-def pasta(request):
-    servings = int(request.GET.get("servings", 1))
-    context = {
-        'recipe': {
-            'макароны, г': 0.3 * servings,
-            'сыр, г': 0.05 * servings,
-        }
-    }
-    return render(request, 'calculator/index.html', context)
-
-
-def buter(request):
-    servings = int(request.GET.get("servings", 1))
-    context = {
-        'recipe': {
-            'хлеб, ломтик': 1 * servings,
-            'колбаса, ломтик': 1 * servings,
-            'сыр, ломтик': 1 * servings,
-            'помидор, ломтик': 1 * servings,
-        }
-    }
-    return render(request, 'calculator/index.html', context)
